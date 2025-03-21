054dd0ac1fa57bb68ea12c0fc544da1c68e1bb55
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-21 17:47:31 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,25 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+	public static int sum2D(int numArray[][]){
+	int sum = 0;
+		for (int row = 0; row < numArray.length; row++) {
+			for (int col = 0; col < numArray[row].length; col++) {
+				sum += numArray[row][col];
+		}
+	}
+	return sum;
+}
+	public static int sumRow(int numArray[][], int rowNum){
+		int sum = 0;
+		for (int col = 0; col < numArray[rowNum].length;col++)
+		{sum += numArray[rowNum][col];}
+		return sum;
+	}
+	public static int sumColumn(int numArray[][], int colNum){
+		int sum = 0;
+		for (int row = 0; row < numArray.length;row++)
+		{sum += numArray[row][colNum];}
+		return sum;
+}
 }
