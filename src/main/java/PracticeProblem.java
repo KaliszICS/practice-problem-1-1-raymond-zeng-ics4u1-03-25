e9baad3a389d77f95f9b05f6dbd273f21402b335
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-21 17:48:19 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,18 +10,18 @@ public class PracticeProblem {
 				sum += numArray[row][col];
 		}
 	}
-	return sum;
+		return sum;
 }
 	public static int sumRow(int numArray[][], int rowNum){
 		int sum = 0;
 		for (int col = 0; col < numArray[rowNum].length;col++)
-		{sum += numArray[rowNum][col];}
+			{sum += numArray[rowNum][col];}
 		return sum;
 	}
 	public static int sumColumn(int numArray[][], int colNum){
 		int sum = 0;
 		for (int row = 0; row < numArray.length;row++)
-		{sum += numArray[row][colNum];}
+			{sum += numArray[row][colNum];}
 		return sum;
 }
 }
